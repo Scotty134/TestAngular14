3f7787c82f3f7885e476a0527ddfa084ffcc6613
scotty134 <arestov.yurii@gmail.com>
2023-01-24 20:39:37 -0600

    initial commit

---
+++ src/app/_models/member.ts
@@ -0,0 +1,16 @@
+import { Photo } from "./photo";
+
+export interface Member {
+    id: number;
+    userName: string;
+    name: string;
+    gender: string;
+    dateOfBirth: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    created: Date;
+    lastActive: Date;
+    photos: Photo[];
+    age: number;
+}
